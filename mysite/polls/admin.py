9bc5c6ec3b005481932de2d1129b5db4b97f70ed
Jeremy0818 <sheeyeowchoo@gmail.com>
2021-02-22 17:21:25 -0700

    firt commit for lab 6

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice, Question
 
-admin.site.register(Choice)
-admin.site.register(Question)
+# admin.site.register(Choice)
+# admin.site.register(Question)
